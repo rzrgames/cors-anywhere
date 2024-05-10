4d00f86f7f48dd8ede5677996fecbf4e3415f023
rzrgames <110288747+rzrgames@users.noreply.github.com>
2024-05-11 03:17:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-   originWhitelist: [],
+   originWhitelist: ['192.168.0.4:8080'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
